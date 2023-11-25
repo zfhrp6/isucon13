cf9c66f9d241e3b248fc7626ac0436b823487dd1
zfhrp <zfhrp6@users.noreply.github.com>
2023-11-25 11:15:40 +0900

    適当にインデックス2

--- sql/initdb.d/10_schema.sql
+++ sql/initdb.d/10_schema.sql
@@ -14,7 +14,8 @@ CREATE TABLE `users` (
 CREATE TABLE `icons` (
   `id` BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   `user_id` BIGINT NOT NULL,
-  `image` LONGBLOB NOT NULL
+  `image` LONGBLOB NOT NULL,
+  INDEX `i_icons_1` (`user_id`)
 ) ENGINE=InnoDB CHARACTER SET utf8mb4 COLLATE utf8mb4_bin;
 
 -- ユーザごとのカスタムテーマ
