6e03eccbc7536d12d4beeedda6ca1dec1c1dfbc1
zfhrp <zfhrp6@users.noreply.github.com>
2023-11-25 11:53:46 +0900

    コメント日時の降順インデックス

--- sql/initdb.d/10_schema.sql
+++ sql/initdb.d/10_schema.sql
@@ -86,7 +86,8 @@ CREATE TABLE `livecomments` (
   `tip` BIGINT NOT NULL DEFAULT 0,
   `created_at` BIGINT NOT NULL,
   INDEX `i_livecomments_1` (`user_id`),
-  INDEX `i_livecomments_2` (`livestream_id`)
+  INDEX `i_livecomments_2` (`livestream_id`),
+  INDEX `i_livecomments_3` (`created_at` DESC)
 ) ENGINE=InnoDB CHARACTER SET utf8mb4 COLLATE utf8mb4_bin;
 
 -- ユーザからのライブコメントのスパム報告
